091ae56a0c4d5c73742ccbb69525a774fd88346a
bodo-gruetter <bodo.gruetter@gmail.com>
2020-05-06 13:41:39 +0200

    a

--- group13_winequality.R
+++ group13_winequality.R
@@ -1,3 +1,17 @@
+# Loading the data into data.frame "d.wine"
 d.wine <- read.csv("winequalityN.csv", header=TRUE)
-head(d.wine)
+
+# Showing the structure of d.wine
 str(d.wine)
+
+# Showing the first and last rows
+head(d.wine)
+tail(d.wine)
+
+# Count and mean the not available (NA) values
+sum(is.na(d.wine))
+mean(is.na(d.wine))
+
+# Omit the rows which contains NAs
+d.wine <- na.omit(d.wine)
+sum(is.na(d.wine))
