c1cc7ca80bc6b4e8d1b753da01eb0147b02c2acf
bodo-gruetter <bodo.gruetter@gmail.com>
2020-05-12 18:50:47 +0200

    commit

---
+++ group13_bikesharing.R
@@ -0,0 +1,4 @@
+d.bike <- read.csv("bikesharing.csv", header=TRUE)
+str(d.bike)
+head(d.bike)
+tail(d.bike)
--- group13_winequality.R
+++ group13_winequality.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+
+##Getting the Data
 # Loading the data into data.frame "d.wine"
 d.wine <- read.csv("winequalityN.csv", header=TRUE)
 
@@ -15,3 +18,40 @@ mean(is.na(d.wine))
 # Omit the rows which contains NAs
 d.wine <- na.omit(d.wine)
 sum(is.na(d.wine))
+
+# Showing the Distribution
+table(d.wine$quality)
+
+##Graphical Analysis
+#simple boxplot with 2 catogorial variables
+##Histogram
+ggplot(data = d.wine, aes(fill=as.factor(d.wine$quality), x = d.wine$fixed.acidity)) +
+  geom_histogram(bins=30, col="black")
+
+##Boxplot
+ggplot(data = d.wine, aes(y = d.wine$quality, x = d.wine$type)) +
+  geom_boxplot()
+
+ggplot(d.wine, aes(x=quality, y=fixed.acidity, fill=type)) + 
+  geom_boxplot() +
+  facet_wrap(~type)
+
+qplot(y = d.wine$quality, x = d.wine$type,
+      data = d.wine,
+      facets = ~ d.wine$type)
+
+ggplot(data = d.wine, aes(y = d.wine$quality, x = d.wine$type)) +
+  geom_boxplot(alpha = 0) +
+  geom_jitter(alpha = 0.1, color = "tomato")
+
+ggplot(data = d.wine, aes(y = d.wine$fixed.acidity, x = d.wine$quality)) +
+  geom_boxplot()
+
+
+##Lab example
+ggplot(data = d.wine, mapping = aes(y = d.wine$quality, x = d.wine$fixed.acidity)) +
+  geom_point()
+
+#
+ggplot(data = d.wine, aes(y = d.wine$quality, x = d.wine$fixed.acidity)) +
+  geom_jitter()
