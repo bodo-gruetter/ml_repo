2699ca9b11b4c624fb4482b8ef386bf48c80edab
bodo-gruetter <bodo.gruetter@gmail.com>
2020-06-05 15:53:31 +0200

    first svm try

--- group13_bikesharing.R
+++ group13_bikesharing.R
@@ -1,4 +1,6 @@
 library(ggplot2)
+#install.packages("e1071")
+library(e1071)     # for the SVM funtion()
 
 d.bike <- read.csv("bikesharing.csv", header=TRUE)
 
@@ -106,3 +108,52 @@ qplot(y = cnt, x = hum, data = d.bike, facets = ~ hr) + geom_smooth()
 
 qplot(y = cnt, x = hum, data = d.bike, facets = ~ weathersit) + geom_smooth()
 
+## SVM
+
+# Linear SVM with cnt and temp
+i <- 720
+s <- -900
+ggplot(data = d.bike, mapping = aes(y = cnt, x = temp)) + geom_point() + geom_abline(intercept = i, slope = s)
+
+# Creating class
+c.bike <- data.frame(x=d.bike, t=as.factor(as.numeric(i + s * d.bike$temp <= d.bike$cnt)))
+ggplot(data = c.bike, mapping = aes(y = x.cnt, x = x.temp,  color=t)) + geom_point() + geom_abline(slope = s, intercept = i, size = 1, alpha = 0.5)
+
+# Creating svm model
+svm.bike.1 <- svm(t~x.cnt+x.temp,
+           data = c.bike,
+           kernel = "linear",
+           cost = 10,
+           scale = FALSE)
+plot(svm.bike.1, c.bike, x.cnt~x.temp)
+
+svm.bike.1$index
+summary(svm.bike.1)
+
+cost_range <-
+  c(0.01,
+    0.1,
+    1,
+    5,
+    10,
+    100)
+tune.out <- tune(
+  svm,
+  t ~ x.cnt+x.temp,
+  data = c.bike,
+  kernel = "linear",
+  ranges = list(cost = cost_range)
+)
+summary(tune.out)
+
+svm.bike.best <- tune.out$best.model
+summary(svm.bike.best)
+plot(svm.bike.best, c.bike, x.cnt~x.temp)
+
+table(predict = predict(svm.bike.best, c.bike),
+      truth = c.bike$t)
+
+# Non linear SVM with cnt and temp
+
+
+# Non linear SVM with developed model
