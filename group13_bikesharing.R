f0f92fa4745a336c5a81ecf052f4e748a3ea7008
bodo-gruetter <bodo.gruetter@gmail.com>
2020-06-07 11:41:09 +0200

    Model development and regression analysis

--- group13_bikesharing.R
+++ group13_bikesharing.R
@@ -1,6 +1,12 @@
 library(ggplot2)
 #install.packages("e1071")
 library(e1071)     # for the SVM funtion()
+library(mgcv)
+
+# clear environment
+rm(list = ls())
+# set plotting window to default
+par(mfrow = c(1, 1))
 
 ########## Getting Data ##########
 
@@ -112,6 +118,83 @@ qplot(y = cnt, x = hum, data = d.bike, facets = ~ hr) + geom_smooth()
 
 qplot(y = cnt, x = hum, data = d.bike, facets = ~ weathersit) + geom_smooth()
 
+########## MODEL DEVELOPMENT ##########
+#model with all predictors
+full.model.1 <- cnt ~ as.factor(season) + as.factor(yr) + as.factor(mnth) + as.factor(hr) + as.factor(holiday) + as.factor(weekday) + as.factor(workingday) + as.factor(weathersit) + temp + atemp + hum + windspeed + casual + registered
+
+#starting model with interaction effects
+starting.model.1 <- cnt ~ as.factor(season) + as.factor(yr) + as.factor(mnth) + as.factor(hr) + as.factor(weathersit) + temp + atemp + hum + season:temp + season:atemp + season:hum + yr:hum + mnth:temp + mnth:atemp + mnth:hum + hr:temp + hr:atemp + hr:hum + weathersit:temp + weathersit:atemp + weathersit:hum
+
+#starting model without interaction effects
+starting.model.2 <- cnt ~ as.factor(season) + as.factor(yr) + as.factor(mnth) + as.factor(hr) + as.factor(weathersit) + temp + atemp + hum
+
+#Comparison
+lm.starting.model.1 <- lm(starting.model.1, data = d.bike)
+lm.starting.model.2 <- lm(starting.model.2, data = d.bike)
+summary(lm.starting.model.1)
+summary(lm.starting.model.2)
+
+#Updating better model (lm.starting.model.1)
+#Drop the interactions without an effect
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - weathersit:atemp)
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - weathersit:temp)
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - hr:atemp)
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - mnth:temp)
+summary(lm.starting.model.1)
+
+#Drop the interactions with weak effect
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - mnth:hum)
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - season:temp)
+summary(lm.starting.model.1)
+
+#Drop the interactions with medium effect
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - weathersit:hum)
+summary(lm.starting.model.1)
+
+#Drop atemp
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - atemp)
+summary(lm.starting.model.1)
+
+#Drop temp
+lm.starting.model.1 <- update(lm.starting.model.1, . ~ . - temp)
+summary(lm.starting.model.1)
+
+#Comparison of lm.starting.model.1 and model with all predictors
+lm.full.model.1 <- lm(full.model.1, data = d.bike)
+summary(lm.starting.model.1)
+summary(lm.full.model.1)
+
+#save starting.model.1 in final model
+final.model <- cnt ~ as.factor(season) + as.factor(yr) + as.factor(mnth) + as.factor(hr) + as.factor(weathersit) + hum + atemp:season + hum:season + hum:yr + atemp:mnth + temp:hr + hum:hr
+
+########## REGRESSION ANALYSIS ##########
+##Linear Regression
+lm.bike.1 <- lm(final.model, data = d.bike)
+summary(lm.bike.1)
+
+#calculate the Root-mean-squared error
+lm.rmse <- sqrt(mean(lm.bike.1$residuals^2))
+lm.rmse
+
+##Non-Linear Regression
+gam.bike.1 <- gam(cnt ~ as.factor(season) + as.factor(yr) + as.factor(mnth) + as.factor(hr) + as.factor(weathersit) + s(hum) + s(temp) + s(atemp) + atemp:season + hum:season + hum:yr + atemp:mnth + temp:hr + hum:hr, data = d.bike)
+summary(gam.bike.1)
+
+#Calculate the Root-mean-squared error
+gam.rmse <- sqrt(mean(gam.bike.1$residuals^2))
+gam.rmse
+
+##Poisson Regression
+poi.bike.1 <- glm(final.model, data = d.bike)
+summary(poi.bike.1)
+
+#Calculate the Root-mean-squared error
+poi.rmse <- sqrt(mean(poi.bike.1$residuals^2))
+poi.rmse
+
+##Compare the models
+
+
 ########## SVM ##########
 
 # Linear SVM with cnt and temp
@@ -134,6 +217,7 @@ plot(svm.bike.1, c.bike, x.cnt~x.temp)
 svm.bike.1$index
 summary(svm.bike.1)
 
+# Build a cost range for tuning
 cost_range <-
   c(0.01,
     0.1,
@@ -141,6 +225,8 @@ cost_range <-
     5,
     10,
     100)
+
+# tune.out model
 tune.out <- tune(
   svm,
   t ~ x.cnt+x.temp,
@@ -150,10 +236,12 @@ tune.out <- tune(
 )
 summary(tune.out)
 
+# get the model with pest cost parameter
 svm.bike.best <- tune.out$best.model
 summary(svm.bike.best)
 plot(svm.bike.best, c.bike, x.cnt~x.temp)
 
+# show if there are any errors in prediction
 table(predict = predict(svm.bike.best, c.bike),
       truth = c.bike$t)
 
