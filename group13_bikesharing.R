6e1f4b27bce62285c3d34f44c853ba8304c93154
bodo-gruetter <bodo.gruetter@gmail.com>
2020-06-08 10:47:04 +0200

    R-File

--- group13_bikesharing.R
+++ group13_bikesharing.R
@@ -4,7 +4,11 @@ library(e1071)     # for the SVM funtion()
 library(mgcv)
 #install.packages("corrplot")
 library(corrplot)
+#install.packages("tree")
+library(tree)
+library(mgcv)
 
+########## Prepare Environment ##########
 # clear environment
 rm(list = ls())
 # set plotting window to default
@@ -45,75 +49,84 @@ predictors = c("season", "yr","mnth","hr","holiday","weekday",
              "workingday","weathersit","temp","atemp","hum","windspeed", "casual", "registered")
 corrplot(cor(data.matrix(d.bike[predictors])))
 
+## Investigating cnt
+ggplot(data = d.bike, aes(x=cnt)) +
+  geom_histogram(bins=30, colour="black", ylab="Frequency") + xlab("Count") + ylab("Frequency")
+mean(d.bike$cnt)
+sd(d.bike$cnt)
 
 ## Investigating the season
 ggplot(data = d.bike, aes(group=season, y = cnt, x = as.factor(season))) +
   geom_boxplot()
 
+lm.season.1 <- lm(cnt ~ as.factor(season), data = d.bike)
+lm.season.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.season.0, lm.season.1)
+
 ## Investigating the year
 ggplot(data = d.bike, aes(group=yr, y = cnt, x = yr)) +
   geom_boxplot()
+
+lm.year.1 <- lm(cnt ~ as.factor(yr), data = d.bike)
+lm.year.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.year.0, lm.year.1)
 
 ## Investigating the month
 ggplot(data = d.bike, aes(group=mnth, y = cnt, x = mnth)) +
   geom_boxplot() + scale_x_discrete(limits=seq(1,12))
 
+lm.month.1 <- lm(cnt ~ as.factor(mnth), data = d.bike)
+lm.month.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.month.0, lm.month.1)
+
 ## Investigating the hour
 ggplot(data = d.bike, aes(group=hr, y = cnt, x = hr)) +
   geom_boxplot() + scale_x_discrete(limits=seq(0,23))
 
-## Investigating the hour
-ggplot(data = d.bike, aes(group=hr, y = cnt, x = hr)) +
-  geom_point() + scale_x_discrete(limits=seq(0,23))
+lm.hr.1 <- lm(cnt ~ as.factor(hr), data = d.bike)
+lm.hr.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.hr.0, lm.hr.1)
 
 ## Investigating the holiday
 ggplot(data = d.bike, aes(group=holiday, y = cnt, x = holiday)) +
   geom_boxplot()
 
+lm.holiday.1 <- lm(cnt ~ as.factor(holiday), data = d.bike)
+lm.holiday.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.holiday.0, lm.holiday.1)
+
 ## Investigating the weekday
 ggplot(data = d.bike, aes(group=weekday, y= cnt, x = weekday)) +
   geom_boxplot()
+
+lm.weekday.1 <- lm(cnt ~ as.factor(weekday), data = d.bike)
+lm.weekday.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.weekday.0, lm.weekday.1)
 
 ## Investigating the workingday
 ggplot(data = d.bike, aes(group=workingday, y = cnt, x = workingday)) +
   geom_boxplot()
 
+lm.workingday.1 <- lm(cnt ~ as.factor(workingday), data = d.bike)
+lm.workingday.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.workingday.0, lm.workingday.1)
+
 ## Investigating the weather situation
 ggplot(data = d.bike, aes(group=weathersit, y = cnt, x = weathersit)) +
   geom_boxplot()
+
+lm.weathersit.1 <- lm(cnt ~ as.factor(weathersit), data = d.bike)
+lm.weathersit.0 <- lm(cnt ~ 1, data = d.bike)
+anova(lm.weathersit.0, lm.weathersit.1)
 
 ## Investigating the temperature
 ggplot(data = d.bike, mapping = aes(y = cnt, x = temp)) +
   geom_point()
 
-lm.temp.1 <- lm(cnt ~ temp, data = d.bike)
-summary(lm.temp.1)
+gam.temp.1 <- gam(cnt ~ s(temp), data = d.bike)
+summary(gam.temp.1)
 
-qplot(y = cnt, x = temp, data = d.bike, facets = ~ season)
-+ geom_abline(intercept=lm.temp.1$coefficients[1], slope=lm.temp.1$coefficients[2], colour="red", size=1)
-
-## Investigating the felt temperature
-ggplot(data = d.bike, mapping = aes(y = cnt, x = atemp)) +
-  geom_point()
-
-## Investigating the humidity
-ggplot(data = d.bike, mapping = aes(y = cnt, x = hum)) +
-  geom_point()
-
-## Investigating the windspeed
-ggplot(data = d.bike, mapping = aes(y = cnt, x = windspeed)) +
-  geom_point()
-
-## Investigating the casual users
-ggplot(data = d.bike, mapping = aes(y = cnt, x = casual)) +
-  geom_point()
-
-## Investigating the registered users
-ggplot(data = d.bike, mapping = aes(y = cnt, x = registered)) +
-  geom_point()
-
-## Investigating Interactions
-
+#Interactions
 qplot(y = cnt, x = temp, data = d.bike, facets = ~ season) + geom_smooth()
 
 qplot(y = cnt, x = temp, data = d.bike, facets = ~ yr) + geom_smooth()
@@ -124,6 +137,14 @@ qplot(y = cnt, x = temp, data = d.bike, facets = ~ hr) + geom_smooth()
 
 qplot(y = cnt, x = temp, data = d.bike, facets = ~ weathersit) + geom_smooth()
 
+## Investigating the felt temperature
+ggplot(data = d.bike, mapping = aes(y = cnt, x = atemp)) +
+  geom_point()
+
+gam.atemp.1 <- gam(cnt ~ s(atemp), data = d.bike)
+summary(gam.atemp.1)
+
+#interactions
 qplot(y = cnt, x = atemp, data = d.bike, facets = ~ season) + geom_smooth()
 
 qplot(y = cnt, x = atemp, data = d.bike, facets = ~ yr) + geom_smooth()
@@ -134,6 +155,14 @@ qplot(y = cnt, x = atemp, data = d.bike, facets = ~ hr) + geom_smooth()
 
 qplot(y = cnt, x = atemp, data = d.bike, facets = ~ weathersit) + geom_smooth()
 
+## Investigating the humidity
+ggplot(data = d.bike, mapping = aes(y = cnt, x = hum)) +
+  geom_point()
+
+gam.hum.1 <- gam(cnt ~ s(hum), data = d.bike)
+summary(gam.hum.1)
+
+#interactions
 qplot(y = cnt, x = hum, data = d.bike, facets = ~ season) + geom_smooth()
 
 qplot(y = cnt, x = hum, data = d.bike, facets = ~ yr) + geom_smooth()
@@ -143,6 +172,28 @@ qplot(y = cnt, x = hum, data = d.bike, facets = ~ mnth) + geom_smooth()
 qplot(y = cnt, x = hum, data = d.bike, facets = ~ hr) + geom_smooth()
 
 qplot(y = cnt, x = hum, data = d.bike, facets = ~ weathersit) + geom_smooth()
+
+## Investigating the windspeed
+ggplot(data = d.bike, mapping = aes(y = cnt, x = windspeed)) +
+  geom_point()
+
+lm.windspeed.1 <- lm(cnt ~ windspeed, data = d.bike)
+summary(lm.windspeed.1)
+
+## Investigating the casual users
+ggplot(data = d.bike, mapping = aes(y = cnt, x = casual)) +
+  geom_point()
+
+gam.casual.1 <- gam(cnt ~ s(casual), data = d.bike)
+summary(gam.casual.1)
+
+## Investigating the registered users
+ggplot(data = d.bike, mapping = aes(y = cnt, x = registered)) +
+  geom_point()
+
+lm.registered.1 <- lm(cnt ~ registered, data = d.bike)
+summary(lm.registered.1)
+
 
 ########## MODEL DEVELOPMENT ##########
 #model with all predictors
@@ -225,16 +276,13 @@ poi.rmse
 
 ##Compare the models
 
-
-########## SVM ##########
-
-########## UPDATING DATASET FOR SVM ##########
-##Remove instant and dteday
+########## UPDATING DATASET FOR CLASSIFICATION ##########
+##Remove features
 str(d.bike)
 d.bike.new <- subset(d.bike, select=-c(holiday, weekday, workingday, temp, atemp, windspeed, casual, registered))
 str(d.bike.new)
-# build 5 classes 0 to 4 according to cnt
 
+# build 5 classes 0 to 4 according to the number cnt
 max(d.bike.new$cnt)
 max(d.bike.new$cnt)/5
 for(i in 1:nrow(d.bike)){
@@ -250,9 +298,6 @@ for(i in 1:nrow(d.bike)){
     d.bike.new$class[i] <- 4
   }
 }
-View(d.bike.new)
-# set seed
-set.seed(123)
 
 # create weighted train (75%) and test (25%) set
 d.bike.train.id <- sample(seq_len(nrow(d.bike.new)),size = floor(0.75*nrow(d.bike.new)))
@@ -268,25 +313,47 @@ nrow(d.bike.test.new)
 str(d.bike.test.new)
 
 
-###### Linear SVM with cnt and temp
-i <- 720
-s <- -900
-ggplot(data = d.bike, mapping = aes(y = cnt, x = temp)) + geom_point() + geom_abline(intercept = i, slope = s)
+########## Decision Trees ##########
+#Classifaction / Regression Tree
+tree.example <- tree(cnt ~ .-class, data = d.bike.train.new)
+plot(tree.example)
+text(tree.example, cex=0.75)
 
-# Creating class
-c.bike <- data.frame(x=d.bike, t=as.factor(as.numeric(i + s * d.bike$temp <= d.bike$cnt)))
-ggplot(data = c.bike, mapping = aes(y = x.cnt, x = x.temp,  color=t)) + geom_point() + geom_abline(slope = s, intercept = i, size = 1, alpha = 0.5)
+#Pruning
+
+#Bagging
+
+#Random Forest
+
+#Boosting
+
+
+########## SVM ##########
+##### Linear SVM with casual and atemp
+ggplot(data = d.bike, mapping = aes(y = casual, x = atemp)) + geom_point()
+xi <- 0.5525
+ggplot(data = d.bike, mapping = aes(y = casual, x = atemp)) + geom_point() + geom_vline(xintercept = xi, color = "blue")
+
+#Build class 0 and 1
+for(i in 1:nrow(d.bike)){
+  if(d.bike$atemp[i] < 0.5525){
+    d.bike$class[i] <- 0
+  } else if (d.bike$atemp[i] >= 0.5525){
+    d.bike$class[i] <- 1
+  }
+}
+
+c.bike <- data.frame(x=d.bike, y=as.factor(d.bike$class))
+ggplot(data = c.bike, mapping = aes(y = x.casual, x = x.atemp,  color=y)) + geom_point() + geom_vline(xintercept = xi, size = 1, alpha = 0.5)
 
 # Creating svm model
 ?svm
-svm.bike.1 <- svm(t~x.cnt+x.temp,
-           data = c.bike,
-           kernel = "linear",
-           cost = 10,
-           scale = FALSE)
-plot(svm.bike.1, c.bike, x.cnt~x.temp)
-
-svm.bike.1$index
+svm.bike.1 <- svm(y~x.casual+x.atemp,
+                  data = c.bike,
+                  kernel = "linear",
+                  cost = 10,
+                  scale = FALSE)
+plot(svm.bike.1, c.bike, x.casual~x.atemp)
 summary(svm.bike.1)
 
 # Build a cost range for tuning
@@ -301,7 +368,7 @@ cost_range <-
 # tune.out model
 tune.out <- tune(
   svm,
-  t ~ x.cnt+x.temp,
+  y ~ x.casual+x.atemp,
   data = c.bike,
   kernel = "linear",
   ranges = list(cost = cost_range)
@@ -309,42 +376,35 @@ tune.out <- tune(
 summary(tune.out)
 
 # get the model with best cost parameter
-svm.bike.best <- tune.out$best.model
-summary(svm.bike.best)
-plot(svm.bike.best, c.bike, x.cnt~x.temp)
+svm.bike.1.best <- tune.out$best.model
+summary(svm.bike.1.best)
+plot(svm.bike.1.best, c.bike, x.casual~x.atemp)
 
 # show if there are any errors in prediction
-table(predict = predict(svm.bike.best, c.bike),
-      truth = c.bike$t)
+table(predict = predict(svm.bike.1.best, c.bike),
+      truth = c.bike$y)
 
-####### SVM with model developed
+
+###### SVM with model developed
 d.bike.train.svm <- data.frame(x=subset(d.bike.train.new, select=-c(class)), y=as.factor(d.bike.train.new$class))
 d.bike.test.svm <- data.frame(x=subset(d.bike.test.new, select=-c(class)), y=as.factor(d.bike.test.new$class))
 
 #linear kernel
-svm.bike.1 <- svm(y ~ .,
+svm.bike.2 <- svm(y ~ .,
                   data = d.bike.train.svm,
                   kernel = "linear",
                   cost = 0.01,
                   scale = FALSE,
 )
-summary(svm.bike.1)
+summary(svm.bike.2)
 
 # calculate performance of svm.bike.1 on d.bike.train.svm
-predict.svm.bike.1.train <- predict(svm.bike.1, d.bike.train.svm)
-table(predict.svm.bike.1.train, d.bike.train.svm$y)
-
-corrects=sum(predict.svm.bike.1.train=as.numeric(d.bike.train.svm$y))
-errors=sum(predict.svm.bike.1.train!=as.numeric(d.bike.train.svm$y))
-(performance_train=corrects/(corrects+errors))
+predict.svm.bike.2.train <- predict(svm.bike.2, d.bike.train.svm)
+table(predict.svm.bike.2.train, d.bike.train.svm$y)
 
 # calculate performance of svm.bike.1 on d.bike.test.svm
-predict.svm.bike.1.test <- predict(svm.bike.1, d.bike.test.svm)
-table(predict.svm.bike.1.test, d.bike.test.svm$y)
-
-corrects=sum(predict.svm.bike.1.test=as.numeric(d.bike.test.svm$y))
-errors=sum(predict.svm.bike.1.test!=as.numeric(d.bike.test.svm$y))
-(performance_test=corrects/(corrects+errors))
+predict.svm.bike.2.test <- predict(svm.bike.2, d.bike.test.svm)
+table(predict.svm.bike.2.test, d.bike.test.svm$y)
 
 # Build a cost range for tuning
 cost_range <-
@@ -368,21 +428,23 @@ tune.out <- tune(
 summary(tune.out)
 
 # fit the best model
-svm.bike.best <- tune.out$best.model
-summary(svm.bike.best)
+svm.bike.2.best <- tune.out$best.model
+summary(svm.bike.2.best)
 
 # calculate performance of svm.bike.best on d.bike.train.svm
-predict.svm.bike.best.train <- predict(svm.bike.best, d.bike.train.svm)
-table(predict.svm.bike.best.train, d.bike.train.svm$y)
+predict.svm.bike.2.best.train <- predict(svm.bike.2.best, d.bike.train.svm)
+table(predict.svm.bike.2.best.train, d.bike.train.svm$y)
 
-corrects=sum(predict.svm.bike.best.train==d.bike.train.svm$y)
-errors=sum(predict.svm.bike.best.train!=d.bike.train.svm$y)
+corrects=sum(predict.svm.bike.2.best.train==d.bike.train.svm$y)
+errors=sum(predict.svm.bike.2.best.train!=d.bike.train.svm$y)
 (performance_train=corrects/(corrects+errors))
 
 # calculate performance of svm.bike.best on d.bike.test.svm
-predict.svm.bike.best.test <- predict(svm.bike.best, d.bike.test.svm)
-table(predict.svm.bike.best.test, d.bike.test.svm$y)
+predict.svm.bike.2.best.test <- predict(svm.bike.2.best, d.bike.test.svm)
+table(predict.svm.bike.2.best.test, d.bike.test.svm$y)
 
-corrects=sum(predict.svm.bike.best.test==d.bike.test.svm$y)
-errors=sum(predict.svm.bike.best.test!=d.bike.test.svm$y)
+corrects=sum(predict.svm.bike.2.best.test==d.bike.test.svm$y)
+errors=sum(predict.svm.bike.2.best.test!=d.bike.test.svm$y)
 (performance_test=corrects/(corrects+errors))
+
+########## NEURONAL NETWORK ##########
